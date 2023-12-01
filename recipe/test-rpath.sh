9eb076121dd746c03e67a681648eba8df2044837
adibbley <adibbley@nvidia.com>
2023-12-01 17:09:34 -0500

    Track test failures

--- recipe/test-rpath.sh
+++ recipe/test-rpath.sh
@@ -4,12 +4,21 @@
 [[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
+errors=""
+
 for lib in `find ${PREFIX}/${targetsDir}/lib -type f`; do
     [[ $lib =~ \.so ]] || continue
 
     rpath=$(patchelf --print-rpath $lib)
     echo "$lib rpath: $rpath"
-    [[ $rpath == "\$ORIGIN" ]] || exit 1
+    [[ $rpath == "\$ORIGIN" ]] || errors+="$lib\n"
 done
 
-exit 0
+if [[ $errors ]]; then
+    echo "The following libraries were found with an unexpected RPATH:"
+    echo -e "$errors"
+
+    exit 1
+else
+    exit 0
+fi
