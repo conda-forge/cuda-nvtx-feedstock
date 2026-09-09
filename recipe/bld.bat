a25915390c1f9d6eb63bef026f37dea8cc3696b6
Christian Sullivan <chrisullivan@nvidia.com>
2026-09-09 16:21:14 -0400

    Update for CUDA 13.4.1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,11 @@
+if "%TARGET_PLATFORM%" == "win-arm64" (
+    set CUDA_ARCH=arm64
+) else (
+    set CUDA_ARCH=x64
+)
+
 if not exist %PREFIX% mkdir %PREFIX%
 mkdir %LIBRARY_INC%\targets
-mkdir %LIBRARY_INC%\targets\x64
+mkdir %LIBRARY_INC%\targets\%CUDA_ARCH%
 
-move include\nvtx3 %LIBRARY_INC%\targets\x64
+move include\nvtx3 %LIBRARY_INC%\targets\%CUDA_ARCH%
